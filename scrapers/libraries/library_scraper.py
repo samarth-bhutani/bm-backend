58eb5e6405ec1287439254a98e4010b5b1d02a48
Tom Cheah <tomcheah@berkeley.edu>
2019-10-13 14:51:14 -0700

    Add names of libraries

--- scrapers/libraries/library_scraper.py
+++ scrapers/libraries/library_scraper.py
@@ -1,36 +1,19 @@
 import requests
 import bs4 as bs
-import pandas as pd
-import os.path
 
 libraries = {}
-libraries["Anthropology Library"] = {}
-libraries["Art History/Classics Library"] = {}
-libraries["BAMPFA Film Library & Study Center"] = {}
-libraries["Bancroft Library/University Archives"] = {}
-libraries["Berkeley Law Library"] = {}
-libraries["Bioscience, Natural Resources & Public Health Library"] = {}
-libraries["Business Library"] = {}
-libraries["Career Counseling Library"] = {}
-libraries["CED Visual Resources Center"] = {}
-libraries["Chemistry and Chemical Engineering Library"] = {}
-libraries["Doe Library"] = {}
-libraries["Earth Sciences & Map Library"] = {}
-libraries["East Asian Library"] = {}
-libraries["Engineering Library"] = {}
-libraries["Environmental Design Library"] = {}
-libraries["Ethnic Studies Library"] = {}
-libraries["Graduate Theological Union Library"] = {}
-libraries["Institute of Governmental Studies Library"] = {}
-libraries["Institute of Transportation Studies Library"] = {}
-libraries["Main (Gardner) Stacks"] = {}
-libraries["East Asian Library"] = {}
-libraries["Engineering Library"] = {}
-libraries["East Asian Library"] = {}
-libraries["Engineering Library"] = {}
-libraries["East Asian Library"] = {}
-libraries["Engineering Library"] = {}
+library_names = ["Anthropology Library", "Art History/Classics Library", "BAMPFA Film Library & Study Center", "Bancroft Library/University Archives", "Berkeley Law Library", "Bioscience, Natural Resources & Public Health Library", "Business Library", "Career Counseling Library",
+"CED Visual Resources Center", "Chemistry and Chemical Engineering Library", "Doe Library", "Earth Sciences & Map Library", "East Asian Library", "Engineering Library", "Environmental Design Library", "Ethnic Studies Library", "Graduate Theological Union Library", "Institute of Governmental Studies Library", "Institute of Transportation Studies Library", "Main (Gardner) Stacks", "Mathematics Statistics Library", "Moffitt Library", "Morrison Library", "Music Library", "Newspapers & Microforms Library", "Optometry and Health Sciences Library", "Physics-Astronomy Library", "Robbins Collection Library", "Social Research Library", "South/Southeast Asia Library"]
 
+library_urls = [] 
+
+library_to_url = {}
+
+#Initializing libraries dictionary 
+for library in library_names:
+    libraries[library] = {}
+
+print(libraries)
 """
 Hours is an array of: (datetime object, open time, close time)
 Ex: (datetime object, 1pm, 5:30pm)
@@ -38,6 +21,13 @@ Ex: (datetime object, 1pm, 5:30pm)
 Get last Sunday
 
 Hardcode dates into URLs for libraries 
+or use specific library page itself:
+http://www.lib.berkeley.edu/libraries/visual-resources-center
 
-
+1. Iterate through list of libraries.
+2. For each library:
+    - Pull Sunday - Saturday data from library specific website
+    - Create array of (datetime object, open time, close time)
+    - Push to libraries dictionary
+3. Push to JSON
 """
