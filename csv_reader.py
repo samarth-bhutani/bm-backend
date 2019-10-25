675a43aad61a8941d2d3d2e2d7c4517599af90d7
Kevin Liu <kevinliu18@berkeley.edu>
2019-10-24 17:58:27 -0700

    CSV to JSOn converter

--- csv_reader.py
+++ csv_reader.py
@@ -4,8 +4,10 @@ import pandas as pd
 
 def read_csv():
     #Read data from file "resources.csv"
-    data = pd.read_csv("/csv_data/resources.csv")
-    data.head()
+    data = pd.read_csv("csv_data/resources.csv")
+    for row in data.iterrows():
+        for i in range(18):
+            print(row[1][i])
 
 if __name__ == "__main__":
-    read_csv
+    read_csv()
