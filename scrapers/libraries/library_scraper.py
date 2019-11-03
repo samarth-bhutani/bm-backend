dc737cf85f7d4f16d2a3df1ba417271ae5754027
Sudarshan Gopalakrishnan <sudarshan.gopal@berkeley.edu>
2019-11-03 14:06:45 -0800

    Fixed helper for libraries and gyms

--- scrapers/libraries/library_scraper.py
+++ scrapers/libraries/library_scraper.py
@@ -2,7 +2,11 @@ import requests
 from bs4 import BeautifulSoup as bs
 import time 
 import json
-import datetime 
+import datetime
+
+import os, sys
+from os.path import dirname, join, abspath
+sys.path.insert(0, abspath(join(dirname(__file__), '..')))
 import helper
 
 libraries = {}
