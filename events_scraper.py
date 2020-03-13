111f5752750abfc2cd5efedaa6b96752fd035195
Monica Tang <monicatang@berkeley.edu>
2020-03-12 22:51:52 -0700

    add labels and descriptions to event scraper

--- events_scraper.py
+++ events_scraper.py
@@ -18,11 +18,23 @@ def get_curr_week_events_url():
     url = url_p1 + curr_date + url_p3
     return url
 
+def clean_str(string):
+    """
+    Decodes unicode characters, replaces duplicate whitespace with single whitespace, and 
+    strips trailing whitespace.
+    Args: 
+        string (str): The string to clean
+    Returns:
+        A new copy of the cleaned string.
+    """
+    decoded = u.unidecode(string)
+    return re.sub('\s+', ' ', decoded).strip()
+
 def parse_subtitles(subtitles, event):
     """
     Parses SUBTITLES and assigns them to the correct attribute in EVENT.
     Args:
-        subtitles (list): list of subtitle items: Category, Date, Time (optional), Location
+        subtitles (list[str]): list of subtitle items: Category, Date, Time (optional), Location
         event (dict): JSON object representing event
     Returns:
         None
@@ -30,25 +42,36 @@ def parse_subtitles(subtitles, event):
     if len(subtitles) < 2:
         return
 
-    subtitles = list(map(str.strip, subtitles))
-    event["category"] = u.unidecode(subtitles[0])
+    subtitles = list(map(clean_str, subtitles))
+    event["category"] = subtitles[0]
 
-    date = u.unidecode(subtitles[1])
-    # Replace all duplicate whitespace with single whitespace
-    date = re.sub('\s+', ' ', date)
+    date = subtitles[1]
     # Add space after comma
     date = re.sub(',\s*', ', ', date)
     event["date"] = date
 
     if len(subtitles) == 3:
-        event["location"] = u.unidecode(subtitles[2])
+        event["location"] = subtitles[2]
     else:
-        event["time"] = u.unidecode(subtitles[2])
-        event["location"] = u.unidecode(subtitles[3])
+        event["time"] = subtitles[2]
+        event["location"] = subtitles[3]
 
-def find_event_labels(event):
-    # FIXME
-    return {}
+def parse_paragraphs(paragraphs, event):
+    labels = {}
+    event["description"] = []
+    for p in paragraphs:
+        label = p.find('label')
+        if label and re.search('Sponsor', label.text):
+            sponsors = p.find_all('a', href=True)
+            labels["sponsors"] = {}
+            for sponsor in sponsors:
+                sponsor_name = clean_str(sponsor.text)
+                labels["sponsors"][sponsor_name] = sponsor['href']
+        elif label and re.search('Speaker', label.text):
+            labels["speakers"] = clean_str(p.find(text=True, recursive=False))
+        else:
+            event["description"].append(clean_str(p.text))
+    event["labels"] = labels
 
 def find_event_status(event):
     # FIXME
@@ -74,24 +97,21 @@ def parse_event(event_row):
                 - speakers: string of speakers and performers
                 - other:
                     - label: description
-            - description: event description
+            - description: (list[str]) event details
             - link: event detail link
             - image: event image url, if available
     """
     event = {}
     title = event_row.find('h3', class_='event-title').find('a', href=True)
     paragraphs = event_row.find_all('p')
-    event["title"] = u.unidecode(title.text)
+    event["title"] = clean_str(title.text)
     event["link"] = "http://events.berkeley.edu/" + title['href']
     if paragraphs:
         subtitles = paragraphs[0].text.split("|")
         parse_subtitles(subtitles, event)
     if len(paragraphs) > 1:
-        # FIXME: Add full description by scraping event subpages
-        description = u.unidecode(paragraphs[-1].text).strip()
-        event["description"] = re.sub('\s+', ' ', description)
+        parse_paragraphs(paragraphs[1:], event)
     event["status"] = find_event_status(event_row)
-    event["labels"] = find_event_labels(event_row)
     event["image"] = None
     return event
 
@@ -104,7 +124,7 @@ def get_events(url):
     """
     events = []
     response = requests.get(url)
-    html = BeautifulSoup(response.text.encode('utf-8','ignore'), 'html.parser')
+    html = BeautifulSoup(response.text.encode('utf-16','ignore'), 'html.parser')
     event_rows = html.find_all('div', class_='event row')
     for event in event_rows:
         events.append(parse_event(event))
