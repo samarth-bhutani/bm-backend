f2a9198469bd5ea9a1e48b769d31de49f0a3b07c
Monica Tang <monicatang@berkeley.edu>
2020-03-25 16:30:47 -0700

    linting

--- occupancy_scraper.js
+++ occupancy_scraper.js
@@ -1,11 +1,11 @@
-const puppeteer = require('puppeteer');
-const Promise = require('bluebird');
-const {Storage} = require('@google-cloud/storage');
-
 "use strict";
 
+const puppeteer = require("puppeteer");
+const Promise = require("bluebird");
+const {Storage} = require("@google-cloud/storage");
+
 const locations = [
-    "Bioscience & Natural Resources Library", 
+    "Bioscience & Natural Resources Library",
     "Business School Library",
     "Doe Memorial Library",
     "C. V. Starr East Asian Library",
@@ -14,9 +14,11 @@ const locations = [
     "Moffitt Library",
     "Jean Gray Hargrove Music Library",
     "Recreational Sports Facility"
-]
+];
 
-// Google place IDs provided by 'https://developers.google.com/places/web-service/place-id'.
+/** Google place IDs provided by
+ * 'https://developers.google.com/places/web-service/place-id'.
+ */
 const ids = [
     "ChIJ_____-B-hYARkH8qFkVlQZA",
     "ChIJmY7BsDp8hYARfG23CBRdZdU",
@@ -27,65 +29,68 @@ const ids = [
     "ChIJxWEsuCZ8hYAR48_Sst45khU",
     "ChIJ__-_ciV8hYARxgPm1ycRiEQ",
     "ChIJ6xOXzCd8hYARJdVJ4oZ_ZRM"
-]
+];
 
 async function getHistogram(url, page) {
-    /** 
-     * Given the url of a location, returns the occupancy data for each day of the week. 
+    /**
+     * Given the url of a location, returns the occupancy data for
+     * each day of the week.
      */
     await page.goto(url);
-    await page.waitForSelector('.section-popular-times-graph');
+    await page.waitForSelector(".section-popular-times-graph");
 
     // Catches console.log messages in page.evaluate() for debugging purposes
-    page.on('console', msg => {
+    page.on("console", function (msg) {
         for (let i = 0; i < msg.args().length; ++i)
             console.log(`${i}: ${msg.args()[i]}`);
     });
 
     const popularTimesHistogram = await page.evaluate(() => {
         const graphs = {};
-        const days = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
-        [...document.querySelectorAll('.section-popular-times-graph')].forEach((graph, i) => {
-            const day = days[i];
-            graphs[day] = {};
-            graphs[day]["live"] = null;
-            graphs[day]["usual"] = [];
-            // Finds where x axis starts
-            let graphStartFromHour;
-            [...graph.querySelectorAll('.section-popular-times-label')].forEach((label, labelIndex) => {
-                if (graphStartFromHour) return;
-                const hourText = label.textContent.trim();
-                graphStartFromHour = hourText.includes('p')
-                    ? 12 + (parseInt(hourText) - labelIndex)
-                    : parseInt(hourText) - labelIndex;
-            });
-            // Finds values from y axis
-            [...graph.querySelectorAll('.section-popular-times-bar')].forEach((bar, barIndex) => {
-                const liveRegex = /(\d+)(\s+)?(%.+\s)(\d+)(\s+)?(%)/
-                const usualRegex = /\d+(\s+)?%/
-                const attr = bar.getAttribute('aria-label')
-                const occupancyMatch = attr.match(usualRegex);
-                const liveOccupancyMatch = attr.match(liveRegex);
-                const maybeHour = graphStartFromHour + barIndex;
-                const hour = maybeHour > 24 ? maybeHour - 24 : maybeHour;
-                
-                if (liveOccupancyMatch && liveOccupancyMatch.length) {
-                    graphs[day]["live"] = {
-                        hour: hour,
-                        occupancyPercent: parseInt(liveOccupancyMatch[1])
-                    };
-                    graphs[day]["usual"].push({
-                        hour: hour,
-                        occupancyPercent: parseInt(liveOccupancyMatch[4])
-                    })
-                } else if (occupancyMatch && occupancyMatch.length) {
-                    graphs[day]["usual"].push({
-                        hour: hour,
-                        occupancyPercent: parseInt(occupancyMatch[0])
+        const days = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
+        [...document.querySelectorAll(".section-popular-times-graph")]
+            .forEach((graph, i) => {
+                const day = days[i];
+                graphs[day] = {};
+                graphs[day].live = null;
+                graphs[day].usual = [];
+                // Finds where x axis starts
+                let graphStartFromHour;
+                [...graph.querySelectorAll(".section-popular-times-label")]
+                    .forEach((label, labelIndex) => {
+                        if (graphStartFromHour) return;
+                        const hourText = label.textContent.trim();
+                        graphStartFromHour = hourText.includes("p") ? 12 + (parseInt(hourText) - labelIndex) 
+                                                : parseInt(hourText) - labelIndex;
                     });
-                }
+                // Finds values from y axis
+                [...graph.querySelectorAll(".section-popular-times-bar")]
+                    .forEach((bar, barIndex) => {
+                        const liveRegex = /(\d+)(\s+)?(%.+\s)(\d+)(\s+)?(%)/;
+                        const usualRegex = /\d+(\s+)?%/;
+                        const attr = bar.getAttribute("aria-label");
+                        const occupancyMatch = attr.match(usualRegex);
+                        const liveOccupancyMatch = attr.match(liveRegex);
+                        const maybeHour = graphStartFromHour + barIndex;
+                        const hour = maybeHour > 24 ? maybeHour - 24
+                                        : maybeHour;
+                        if (liveOccupancyMatch && liveOccupancyMatch.length) {
+                            graphs[day].live = {
+                                hour: hour,
+                                occupancyPercent: parseInt(liveOccupancyMatch[1])
+                            };
+                            graphs[day].usual.push({
+                                hour: hour,
+                                occupancyPercent: parseInt(liveOccupancyMatch[4])
+                            });
+                        } else if (occupancyMatch && occupancyMatch.length) {
+                            graphs[day].usual.push({
+                                hour: hour,
+                                occupancyPercent: parseInt(occupancyMatch[0])
+                            });
+                        }
+                    });
             });
-        });
         return graphs;
     });
     return popularTimesHistogram;
@@ -102,18 +107,19 @@ async function getData() {
      }
      */
     try {
-        result = {}
-        const URL_BASE = "https://www.google.com/maps/search/?api=1&query=Google&query_place_id="
+        const result = {};
+        const URL_BASE = "https://www.google.com/maps/search/?api=1&query=Google&query_place_id=";
         const browser = await puppeteer.launch({
-            args: ['--no-sandbox'],
+            args: ["--no-sandbox"],
             headless: true
         });
         const page = await browser.newPage();
-        for (i = 0; i < locations.length; i++) {
+        for (let i = 0; i < locations.length; i++) {
             const url = URL_BASE + ids[i];
-            await getHistogram(url, page).then((histogram) => {
-                    result[locations[i]] = histogram;
-                });
+            const histogram = await getHistogram(url, page);
+            if (histogram) {
+                result[locations[i]] = histogram;
+            }
         }
         await page.close();
         await browser.close();
@@ -126,15 +132,15 @@ async function getData() {
 exports.scrape = (req, res) => {
     getData().then((result) => {
         const storage = new Storage();
-        const bucket = storage.bucket('bm-backend-scrap');
-        const file = bucket.file('occupancy.json');
+        const bucket = storage.bucket("bm-backend-scrap");
+        const file = bucket.file("occupancy.json");
         const contents = JSON.stringify(result);
         file.save(contents, function(err) {
             if (err) {
                 console.log(err);
             } else {
-                res.status(200).send('Completed.');
+                res.status(200).send("Completed.");
             }
         });
     });
-}
+};
