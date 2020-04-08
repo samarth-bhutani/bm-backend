e9c0d8ac42eb37d2378f82ec6de10b44a9dc8609
Tom Cheah <tomcheah@berkeley.edu>
2020-04-07 23:13:52 -0700

    Change to cloud pushing

--- firebase_push.py
+++ firebase_push.py
@@ -18,13 +18,13 @@ def update_credentials():
         On cloud functions you don't need to call initialize app or use credentials
     '''
     # for local pushing
-    parent_working_directory = os.path.dirname((abspath(__file__)))
-    cred = credentials.Certificate(parent_working_directory + "/berkeley-mobile-backend-firebase-key.json")
-    firebase_admin.initialize_app(cred)
-    db = firestore.client()
+#     parent_working_directory = os.path.dirname((abspath(__file__)))
+#     cred = credentials.Certificate(parent_working_directory + "/berkeley-mobile-backend-firebase-key.json")
+#     firebase_admin.initialize_app(cred)
+#     db = firestore.client()
 
     # for cloud-function pushing 
-    # db = firestore.Client()
+    db = firestore.Client()
     return db 
 
 def scrape_dining_hall_information(db):    
