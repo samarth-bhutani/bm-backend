7431ef3118fae3578655536cfe54b5ce01c9e1da
Tom Cheah <tomcheah@berkeley.edu>
2020-04-07 22:33:12 -0700

    Update firebase_push.py

--- firebase_push.py
+++ firebase_push.py
@@ -18,13 +18,13 @@ def update_credentials():
         On cloud functions you don't need to call initialize app or use credentials
     '''
     # for local pushing
-    # parent_working_directory = os.path.dirname((abspath(__file__)))
-    # cred = credentials.Certificate(parent_working_directory + "/berkeley-mobile-backend-firebase-key.json")
-    # firebase_admin.initialize_app(cred)
-    # db = firestore.client()
+    parent_working_directory = os.path.dirname((abspath(__file__)))
+    cred = credentials.Certificate(parent_working_directory + "/berkeley-mobile-backend-firebase-key.json")
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
 
     # for cloud-function pushing 
-    db = firestore.Client()
+    # db = firestore.Client()
     return db 
 
 def scrape_dining_hall_information(db):    
@@ -77,14 +77,14 @@ def scrape_library_information(db):
 
 def scrape_resources_information(db):
     '''
-        Runs the resources scraper and updates Firebase with new data
+        Run the resources scraper and updates Firebase with new data
     '''
     resources = resources_scraper.scrape()
     for resource_types in resources:
         resource_collection = db.collection(str(resource_types))
         for resource in resources[resource_types]:
             resource_collection.document(str(resource)).set(resources[resource_types][resource])
-        
+
 def scrape_events_information(db):
     '''
         Runs the events scraper and updates Firebase with new data
@@ -93,7 +93,7 @@ def scrape_events_information(db):
     event_collection = db.collection(u'Events')
     for day in events:
         events_collection.document(day).set(events[day])
-
+        
 def log_time(db):
     '''
         Logs the last time firebase has been updated
