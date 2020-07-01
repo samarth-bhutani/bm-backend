6d5febc20115a3e3e39a967914f903192ecc3669
Sudarshan Gopalakrishnan <sudarshan.gopal@bekeley.edu>
2020-06-30 18:34:36 -0700

    Suspending Cafe Scraper since it has been migrated to resources

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ def scrape_and_push(request):
     firebase_push.scrape_dining_hall_information(db)
     print("Dining hall update complete!")
 
-    print("Updating cafes...")
-    firebase_push.scrape_cafe_information(db)
-    print("Cafe update complete!")
-    
+    # print("Updating cafes...")
+    # firebase_push.scrape_cafe_information(db)
+    # print("Cafe update complete!")
+
     print("Updating libraries...")
     firebase_push.scrape_library_information(db)
     print("Libraries update complete!")
@@ -24,17 +24,17 @@ def scrape_and_push(request):
     print("Updating resources...")
     firebase_push.scrape_resources_information(db)
     print("Resources update complete!")
-    
+
     print("Updating events...")
     firebase_push.scrape_events_information(db)
     print("Events update complete!")
 
     print("Updating gyms...")
-    firebase_push.scrape_gym_information(db) 
+    firebase_push.scrape_gym_information(db)
     print("Gyms update complete!")
 
     print("Updating gym classes...")
-    firebase_push.scrape_gym_classes_information(db) 
+    firebase_push.scrape_gym_classes_information(db)
     print("Gym classes update complete!")
 
 scrape_and_push("a")
